97069a7bd7da6f03c9cf92895bb9800d42ecd72f
samar verma <samarverma004@gmail.com>
2024-06-30 23:42:04 +0530

    promises

---
+++ 09-asychronous-javascript/08-promise-practice/script.js
@@ -0,0 +1,29 @@
+const posts = [
+    {name: 'samar verma', age: 22},
+    {name: 'chirag tawar', age: 21}
+];
+
+
+const post = new Promise( (resolve) => {
+   setTimeout (() => {
+    item = {name: 'raja', age: 23};
+    posts.push(item);
+    resolve();
+},1000);
+});
+
+
+const getPost = () => {
+    const list =  document.getElementById('list');
+    posts.forEach((item) => {
+    const post = document.createElement('li');
+    post.innerHTML = `<h>${item.name} - ${item.age}</h1>`
+    list.appendChild(post);
+   });
+};
+
+
+post.then(getPost);
+
+
+
---
+++ 09-asychronous-javascript/10-promise-all/script.js
@@ -0,0 +1,6 @@
+// Promise.all is a method that takes an array of promises and 
+// returns a single promise that resolves when all of the promises
+//  in the array have resolved or rejects when any one of the promises 
+//  in the array rejects. This is useful for performing multiple 
+//  asynchronous operations concurrently and handling their results 
+//  collectively.
